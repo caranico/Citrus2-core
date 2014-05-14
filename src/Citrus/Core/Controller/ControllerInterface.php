a7776644fed684008949d70975dada76e5ad5ad2
Rémi <remi@caramia.fr>
2014-05-14 19:29:48 +0200

    removing double source directory

--- src/Citrus/Core/Controller/ControllerInterface.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace Citrus\Core\Controller;
-
-interface ControllerInterface
-{
-
-    public function render($args, $view = null);
-}
--- src/Citrus/Core/System/ServiceProviderInterface.php
+++
@@ -1,9 +0,0 @@
-<?php
-namespace Citrus\Core\System;
-
-interface ServiceProviderInterface
-{
-    public function register($app);
-
-    public function boot($app);
-}
