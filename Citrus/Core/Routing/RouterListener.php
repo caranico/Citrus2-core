b3598ead0bc5d4d6db89f859ac8cb05e6574ee01
Rémi <remi@caramia.fr>
2014-05-15 17:59:28 +0200

    namespace fix

--- Citrus/Core/Routing/RouterListener.php
+++ Citrus/Core/Routing/RouterListener.php
@@ -5,6 +5,7 @@ namespace Citrus\Core\Routing;
 use Symfony\Component\EventDispatcher\EventSubscriberInterface;
 use Symfony\Component\HttpKernel\Event\GetResponseEvent;
 use Symfony\Component\HttpKernel\KernelEvents;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 
 class RouterListener implements EventSubscriberInterface
 {
